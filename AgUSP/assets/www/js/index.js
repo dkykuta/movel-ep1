bc0b024819fc3009099e85df36e49017324ddb88
Diogo Haruki <dkykuta@gmail.com>
2013-04-09 21:55:12 -0300

    FEFZ, FEFZ GUARANA FEFZ, pegando feed

--- AgUSP/assets/www/js/index.js
+++ AgUSP/assets/www/js/index.js
@@ -1,18 +1,33 @@
 var app = {
-    initialize: function() {
-        this.bindEvents();
+	url: "http://www.usp.br/agen/?feed=rss2",
+	
+	entriesCache: {},
+		
+    addFeedItem: function(string) {
+    	var container = document.getElementById("feedDiv");
+	    var div = document.createElement("div");
+	    div.appendChild(document.createTextNode(string));
+	    div.setAttribute("class", "feedItem");
+        container.appendChild(div);
     },
     
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    getResults: function() {
+    	var feed = new google.feeds.Feed(app.url);
+    	feed.setNumEntries(7);
+    	feed.load(function(result) {
+    		if (!result.error) {
+    			for (var i = 0; i < result.feed.entries.length; i++) {
+    				var entry = result.feed.entries[i];
+    				app.addFeedItem(entry.title);
+    			}
+    		}
+    	});
     },
-    
-    onDeviceReady: function() {
-    	document.addEventListener('online', this.onOnline, false);
-        app.receivedEvent('deviceready');
-    },
-    
-    onOnline: function() {
-    	// FAZ BACKUP
+	    
+
+    googleFeedInitialize: function(url) {
+    	google.load("feeds", "1");
+    	
+    	google.setOnLoadCallback(app.getResults);
     }
 };
