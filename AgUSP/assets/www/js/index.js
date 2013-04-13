9ce8bceccb463674e91a8e4639265f5d29b15864
Fernando <fernando_aluani@hotmail.com>
2013-04-13 20:44:00 -0300

    Save/Load feeds do cache implementado; Refresh implementado; get/set de numero de feed a ser mostrado tb foi implementado, salvando valor no cache;

--- AgUSP/assets/www/js/index.js
+++ AgUSP/assets/www/js/index.js
@@ -3,6 +3,14 @@ var app = {
 	
 	entriesCache: {},
 		
+	addFeeds: function(container, feeds) {
+		for (var i = 0; i < feeds.length; i++) {
+			var entry = feeds[i];
+			app.addFeedItem(container, entry);
+		}
+		container.trigger( "create" );
+	},
+	
     addFeedItem: function(container, feedItem) {
 	    var string = "<div data-role=\"collapsible\"><h3>" + feedItem.title +
 	    "</h3>" + feedItem.content + "</div>";
@@ -10,25 +18,77 @@ var app = {
     },
     
     getResults: function() {
-    	var feed = new google.feeds.Feed(app.url);
-    	feed.setNumEntries(7);
-    	feed.load(function(result) {
-    		var container = $("#feedDiv");
-    		if (!result.error) {
-    			for (var i = 0; i < result.feed.entries.length; i++) {
-    				var entry = result.feed.entries[i];
-    				app.addFeedItem(container, entry);
-    			}
-    			container.trigger( "create" );
-    		}
-    	});
+    	var container = $("#feedDiv");
+    	var cacheFeeds = app.settings.getCachedFeed();
+    	if (cacheFeeds!=null) {
+    		console.log("LOADING FROM CACHE");
+    		app.addFeeds(container, cacheFeeds);
+    	}
+    	else { 
+    		console.log("USING FEED API TO LOAD");
+    		var feed = new google.feeds.Feed(app.url);
+    		feed.includeHistoricalEntries();
+        	feed.setNumEntries(app.settings.getNumFeedEntries());
+        	feed.load(function(result) {
+        		if (!result.error) {
+        			app.addFeeds(container, result.feed.entries);
+        			app.settings.saveFeed(result.feed.entries);
+        		}
+        	});
+    	}
     },
 	    
+    refresh: function() {
+    	console.log("REFRESHING");
+    	app.settings.removeCachedFeed();
+    	var container = $("#feedDiv");
+    	container.empty(); //this removes all childs of container, leaving it empty
+    	app.getResults();
+    },
 
     googleFeedInitialize: function(url) {
     	google.load("feeds", "1");
     	app.url = url;
     	
-    	google.setOnLoadCallback(app.getResults);
+    	//use isso pra limpar o cache
+    	//localStorage.clear();
+    	//  ou
+    	//localStorage.removeItem(key)
+		google.setOnLoadCallback(app.getResults);
+    },
+    
+    /////////////////////////////////////////////////////////////////////
+    // holds getters and setters for config settings and persistent data.
+    settings: {
+    	FEED_CACHE_KEY: "feedCache",
+    	NUM_ENTRIES_KEY: "numEntries",
+    
+        saveFeed: function(feed) {
+        	localStorage.setItem(app.settings.FEED_CACHE_KEY, JSON.stringify(feed));
+        },
+        
+        getCachedFeed: function() {
+        	var cache = localStorage.getItem(app.settings.FEED_CACHE_KEY);
+        	if (cache != null) {
+        		return JSON.parse(cache);
+        	}
+        	return null;
+        },
+        
+        removeCachedFeed: function() {
+        	localStorage.removeItem(app.settings.FEED_CACHE_KEY);
+        },
+        
+        setNumFeedEntries: function(num) {
+        	localStorage.setItem(app.settings.NUM_ENTRIES_KEY, num);
+        },
+        
+        getNumFeedEntries: function() {
+        	var num = localStorage.getItem(app.settings.NUM_ENTRIES_KEY);
+        	if (num != null) {
+        		return parseInt(num);
+        	}
+        	return 20; //default value
+        },
     }
 };
